d35477442152ef7f4dd63d8bac88614d95519896
phamducbinhh <phamducbinh1712000@gmail.com>
2023-12-29 10:27:24 +0700

    Update Css Baseline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,7 +35,7 @@ module.exports = {
   rules: {
     // Tắt rule yêu cầu import React trong file jsx
     'react/react-in-jsx-scope': 'off',
-    // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
+    // Cảnh báo khi thẻ <a target='_blank'> mà không có rel='noreferrer'
     'react/jsx-no-target-blank': 'warn',
     // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
     'prettier/prettier': [
@@ -50,6 +50,13 @@ module.exports = {
         singleQuote: true,
         printWidth: 120,
         jsxSingleQuote: true
+      }
+    ],
+    
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
       }
     ]
   }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
