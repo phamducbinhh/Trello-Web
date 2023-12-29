f06eb67a0c52a735e54e9ddaec110d90457f6fa0
phamducbinhh <phamducbinh1712000@gmail.com>
2023-12-29 10:49:56 +0700

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
 import App from './App.tsx'
+import theme from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
